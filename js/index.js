b9c86cbdc311d8a867e1b7bed4e3261d4a55d218
fromwhence <wenc.matt@gmail.com>
2021-02-13 11:27:53 -0600

    Added prettier and improved celeb mutliple choices events

--- js/index.js
+++ js/index.js
@@ -1,40 +1,40 @@
-"use strict";
+'use strict';
 
-const topBar = document.querySelector(".topbar");
-const quizContent = document.getElementById("quiz-content");
+const topBar = document.querySelector('.topbar');
+const quizContent = document.getElementById('quiz-content');
 
 // Modals and related buttons
-const startQuizBtn = document.querySelector(".start-btn");
-const modal = document.querySelector(".modal");
-const endModal = document.querySelector(".end-modal");
-const endModalContent = document.querySelector(".end-modal-window");
-const closeEndModal = document.querySelector(".close-end-modal");
-const howToPlay = document.querySelector(".how-to-play");
-const tryAgainLink = document.querySelector(".try-again");
-const tryAgainBtn = document.querySelector(".try-again-btn");
+const startQuizBtn = document.querySelector('.start-btn');
+const modal = document.querySelector('.modal');
+const endModal = document.querySelector('.end-modal');
+const endModalContent = document.querySelector('.end-modal-window');
+const closeEndModal = document.querySelector('.close-end-modal');
+const howToPlay = document.querySelector('.how-to-play');
+const tryAgainLink = document.querySelector('.try-again');
+const tryAgainBtn = document.querySelector('.try-again-btn');
 
 // Multiple choice data
-const celebrityImage = document.querySelector(".celebrity-image");
-const choicesArr = Array.from(document.querySelectorAll(".choice-text"));
+const celebrityImage = document.querySelector('.celebrity-image');
+const choicesArr = Array.from(document.querySelectorAll('.choice-text'));
 const choiceContainerArr = Array.from(
-  document.querySelectorAll(".choice-container")
+  document.querySelectorAll('.choice-container')
 );
-const answerContainer = document.querySelector(".answer-items");
+const answerContainer = document.querySelector('.answer-items');
 
 // Hud data
-const questionCounterText = document.querySelector(".question-counter");
-const progressBarFull = document.querySelector(".progress-bar-full");
-const scoreText = document.querySelector(".score");
-const hudNextArrow = document.querySelector(".hud-next-arrow");
-const nextBtn = document.querySelector(".next-btn");
+const questionCounterText = document.querySelector('.question-counter');
+const progressBarFull = document.querySelector('.progress-bar-full');
+const scoreText = document.querySelector('.score');
+const hudNextArrow = document.querySelector('.hud-next-arrow');
+const nextBtn = document.querySelector('.next-btn');
 
 // End of game data
-const finalScore = document.querySelector(".final-score");
-const endOfGameHeading = document.querySelector(".end-heading");
-const endOfGameText = document.querySelector(".end-paragraph");
+const finalScore = document.querySelector('.final-score');
+const endOfGameHeading = document.querySelector('.end-heading');
+const endOfGameText = document.querySelector('.end-paragraph');
 // const shareLink = document.getElementById("share-link");
 
-// Quiz play variables
+// Quiz data variables
 let currentQuestion = {};
 let questionCounter = 0;
 let availableQuestions = [];
@@ -49,13 +49,13 @@ const maxScore = maxQuestions * 5;
 const topbarHeight = topBar.offsetHeight;
 
 const addShadowOnScroll = () => {
-  topBar.classList.add("scroll-shadow");
+  topBar.classList.add('scroll-shadow');
 };
 const removeShadow = () => {
-  topBar.classList.remove("scroll-shadow");
+  topBar.classList.remove('scroll-shadow');
 };
 
-window.addEventListener("scroll", function () {
+window.addEventListener('scroll', function () {
   const scrollPosition = window.scrollY;
   if (scrollPosition >= topbarHeight) {
     addShadowOnScroll();
@@ -65,32 +65,32 @@ window.addEventListener("scroll", function () {
 });
 
 // Scroll to top after correct answer on small devices
-const topOfPage = document.querySelector(".top");
+const topOfPage = document.querySelector('.top');
 
 const scrollToTop = () => {
-  topOfPage.scrollIntoView({ behavior: "smooth" });
+  topOfPage.scrollIntoView({ behavior: 'smooth' });
   removeShadow();
 };
 
 const imageTransition = () => {
-  celebrityImage.classList.add("fade-in");
+  celebrityImage.classList.add('fade-in');
   setTimeout(function () {
-    celebrityImage.classList.remove("fade-in");
+    celebrityImage.classList.remove('fade-in');
   }, 1000);
   celebrityImage.style.opacity = 1;
 };
 
 const defaultNextButton = () => {
-  hudNextArrow.classList.remove("active");
-  nextBtn.classList.remove("active");
+  hudNextArrow.classList.remove('active');
+  nextBtn.classList.remove('active');
 };
 const activateNextButton = () => {
-  hudNextArrow.classList.add("active");
-  nextBtn.classList.add("active");
+  hudNextArrow.classList.add('active');
+  nextBtn.classList.add('active');
 };
 
 const startQuiz = () => {
-  startQuizBtn.addEventListener("click", scrollToTop);
+  startQuizBtn.addEventListener('click', scrollToTop);
   questionCounter = 0;
   score = 0;
   availableQuestions = [...questions];
@@ -103,22 +103,22 @@ const resetNextQuestion = () => {
   scrollToTop();
   defaultNextButton();
   for (let i = 0; i < choiceContainerArr.length; i++) {
-    choiceContainerArr[i].classList.remove("correct", "incorrect", "inactive");
-    choiceContainerArr[i].lastElementChild.classList.remove("inactive");
+    choiceContainerArr[i].classList.remove('correct', 'incorrect', 'inactive');
+    choiceContainerArr[i].lastElementChild.classList.remove('inactive');
   }
-  answerContainer.style.pointerEvents = "auto";
+  answerContainer.style.pointerEvents = 'auto';
 };
 
 // Upon correct answer, gray out incorrect choices.
 const inactivateNames = () => {
   for (let i = 0; i < choiceContainerArr.length; i++)
-    if (!choiceContainerArr[i].classList.contains("correct", "incorrect")) {
-      choiceContainerArr[i].classList.add("inactive");
+    if (!choiceContainerArr[i].classList.contains('correct', 'incorrect')) {
+      choiceContainerArr[i].classList.add('inactive');
     }
 };
 
 // Shuffle celebrity names for each question
-const shuffleNames = (names) => {
+const shuffleNames = names => {
   for (let i = names.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * i);
     const temp = names[i];
@@ -137,9 +137,9 @@ const getNewQuestion = () => {
 
   let choicesToShuffle = [];
   for (let i = 0; i < choicesArr.length; i++) {
-    const number = choicesArr[i].dataset["number"];
+    const number = choicesArr[i].dataset['number'];
     choicesToShuffle.push(
-      (choicesArr[i].textContent = currentQuestion["choice" + number])
+      (choicesArr[i].textContent = currentQuestion['choice' + number])
     );
   }
 
@@ -156,117 +156,118 @@ const getNewQuestion = () => {
   celebrityImage.style.filter = `blur(${blurAmount}px)`;
 };
 
-choicesArr.forEach((choice) => {
-  choice.addEventListener("click", (e) => {
-    const selectedCeleb = e.target;
-    // console.log(selectedCeleb.parentElement);
-    let classToApply = "incorrect";
+choiceContainerArr.forEach(choice => {
+  choice.addEventListener('click', e => {
+    let selectedCeleb = e.target.parentElement;
+    selectedCeleb = selectedCeleb.lastElementChild;
+    console.log(selectedCeleb);
+    let classToApply = 'incorrect';
     if (selectedCeleb.textContent === currentQuestion.answer) {
-      classToApply = "correct";
+      classToApply = 'correct';
     }
     selectedCeleb.parentElement.classList.add(classToApply);
 
-    if (classToApply === "correct") {
+    if (classToApply === 'correct') {
       incrementScore(availablePoints);
       activateNextButton();
-      celebrityImage.style.filter = "blur(0)";
-      answerContainer.style.pointerEvents = "none";
+      celebrityImage.style.filter = 'blur(0)';
+      answerContainer.style.pointerEvents = 'none';
       scrollToTop();
       inactivateNames();
     }
 
-    if (classToApply === "incorrect") {
+    if (classToApply === 'incorrect') {
       availablePoints--;
       blurAmount -= 7;
       celebrityImage.style.filter = `blur(${blurAmount}px)`;
     }
 
-    if (classToApply === "incorrect" && blurAmount === 0) {
+    if (classToApply === 'incorrect' && blurAmount === 0) {
       incrementScore(availablePoints);
       activateNextButton();
       scrollToTop();
-      let choices = document.getElementsByClassName("choice-container");
+      let choices = document.getElementsByClassName('choice-container');
       const choicesArr = [...choices];
       for (let i = 0; i < choicesArr.length; i++) {
-        if (choicesArr[i].classList.contains !== "incorrect") {
-          choicesArr[i].classList.add("correct");
+        if (choicesArr[i].classList.contains !== 'incorrect') {
+          choicesArr[i].classList.add('correct');
         }
       }
     }
 
     if (
-      classToApply === "incorrect" &&
+      classToApply === 'incorrect' &&
       blurAmount === 0 &&
       availableQuestions.length == 0
     ) {
       lastQuestion();
     }
 
-    if (classToApply === "correct" && availableQuestions.length == 0) {
+    if (classToApply === 'correct' && availableQuestions.length == 0) {
       lastQuestion();
       inactivateNames();
     }
   });
 });
 
-startQuizBtn.addEventListener("click", function () {
-  modal.classList.remove("active");
+startQuizBtn.addEventListener('click', function () {
+  modal.classList.remove('active');
 });
-howToPlay.addEventListener("click", function () {
-  modal.classList.add("active");
-  startQuizBtn.textContent = "Resume";
+howToPlay.addEventListener('click', function () {
+  modal.classList.add('active');
+  startQuizBtn.textContent = 'Resume';
 });
-closeEndModal.addEventListener("click", function () {
-  endModal.classList.remove("active");
+closeEndModal.addEventListener('click', function () {
+  endModal.classList.remove('active');
 });
-hudNextArrow.addEventListener("click", getNewQuestion);
-nextBtn.addEventListener("click", getNewQuestion);
+hudNextArrow.addEventListener('click', getNewQuestion);
+nextBtn.addEventListener('click', getNewQuestion);
 
-const incrementScore = (availablePoints) => {
+const incrementScore = availablePoints => {
   score += availablePoints;
   scoreText.innerHTML = `Score: ${score}/${maxScore}`;
 };
 
 const endQuizModal = () => {
-  endModal.classList.add("active");
-  endModalContent.classList.add("active");
+  endModal.classList.add('active');
+  endModalContent.classList.add('active');
   finalScore.innerHTML = `${score}/${maxScore}`;
   if (score >= maxScore - 5) {
-    endOfGameHeading.textContent = "Excellent job!";
-    endOfGameText.textContent = "You have a keen eye for celebrities!";
+    endOfGameHeading.textContent = 'Excellent job!';
+    endOfGameText.textContent = 'You have a keen eye for celebrities!';
   } else if (score >= maxScore - 10) {
-    endOfGameHeading.textContent = "Great job!";
-    endOfGameText.textContent = "You have quite an eye for celebrities!";
+    endOfGameHeading.textContent = 'Great job!';
+    endOfGameText.textContent = 'You have quite an eye for celebrities!';
   } else if (score >= maxScore - 15) {
-    endOfGameHeading.textContent = "Good job!";
-    endOfGameText.textContent = "You sure know your celebrities!";
+    endOfGameHeading.textContent = 'Good job!';
+    endOfGameText.textContent = 'You sure know your celebrities!';
   } else if (score >= maxScore - 20) {
-    endOfGameHeading.textContent = "Not bad!";
-    endOfGameText.textContent = "Thanks for playing!";
+    endOfGameHeading.textContent = 'Not bad!';
+    endOfGameText.textContent = 'Thanks for playing!';
   } else {
-    endOfGameHeading.textContent = "Nice try!";
-    endOfGameText.textContent = "You might consider playing again.";
+    endOfGameHeading.textContent = 'Nice try!';
+    endOfGameText.textContent = 'You might consider playing again.';
   }
 };
 
 const lastQuestion = () => {
-  hudNextArrow.classList.remove("active");
-  howToPlay.classList.remove("active");
-  tryAgainLink.classList.add("active");
-  nextBtn.classList.remove("active");
+  hudNextArrow.classList.remove('active');
+  howToPlay.classList.remove('active');
+  tryAgainLink.classList.add('active');
+  nextBtn.classList.remove('active');
   window.setTimeout(endQuizModal, 2000);
 };
 
 const tryAgain = () => {
-  tryAgainLink.classList.remove("active");
-  howToPlay.classList.add("active");
-  endModal.classList.remove("active");
+  tryAgainLink.classList.remove('active');
+  howToPlay.classList.add('active');
+  endModal.classList.remove('active');
   scoreText.innerHTML = `Score: 0/50`;
   startQuiz();
   scrollToTop();
 };
 
-tryAgainBtn.addEventListener("click", tryAgain);
-tryAgainLink.addEventListener("click", tryAgain);
+tryAgainBtn.addEventListener('click', tryAgain);
+tryAgainLink.addEventListener('click', tryAgain);
 
 startQuiz();
---
+++ js/no-focus.js
@@ -0,0 +1,23 @@
+"use-strict";
+// Removes :focus outline for mouse users
+(function (document, window) {
+  if (!document || !window) {
+    return;
+  }
+  const styleText =
+    "::-moz-focus-inner{border:0 !important;}:focus{outline: none !important;";
+  const unfocus_style = document.createElement("STYLE");
+  window.unfocus = function () {
+    document.getElementsByTagName("HEAD")[0].appendChild(unfocus_style);
+    document.addEventListener("mousedown", function () {
+      unfocus_style.innerHTML = styleText + "}";
+    });
+    document.addEventListener("keydown", function () {
+      unfocus_style.innerHTML = "";
+    });
+  };
+  unfocus.style = function (style) {
+    styleText += style;
+  };
+  unfocus();
+})(document, window);
